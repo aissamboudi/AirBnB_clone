17019a27febf9c61e6ed3969ed6c7c2feafa524b
aissamboudi <aissamboudi@gmail.com>
2024-05-19 13:31:31 +0100

    Fix

--- models/base_model.py
+++ models/base_model.py
@@ -36,4 +36,5 @@ class BaseModel:
 
     def __str__(self):
         """ """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}"
+        .format(self.__class__.__name__, self.id, self.__dict__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,5 +4,7 @@
 import json
 from models.base_model import BaseModel
 
+
 class FileStorage:
     """This is Class of Storage """
+    pass
