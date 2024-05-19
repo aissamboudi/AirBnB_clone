0f8892147cb332d9d395f5865a69319b2137fec3
aissamboudi <aissamboudi@gmail.com>
2024-05-19 13:55:58 +0100

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""This is base model class"""
+"""
+This is base model class
+"""
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """BaseModel"""
+    """
+    BaseModel attributes :
+    *args will not be used
+    **kwargs will be used
+    """
     def __init__(self, *args, **kwargs):
         """init of new BaseModel
         kwargs argument used for for the constructor of a BaseModel
@@ -24,10 +30,11 @@ class BaseModel:
                 setattr(self, key, value)
 
     def save(self):
+        """update updated_at with current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ """
+        """return a dictionary with each key,value included in  __dict__ instance """
         idict = self.__dict__.copy()
         idict["__class__"] = self.__class__.__name__
         idict["created_at"] = self.created_at.isoformat()
@@ -35,7 +42,7 @@ class BaseModel:
         return idict
 
     def __str__(self):
-        """ """
+        """return a string represent the object"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
         self.id,
         self.__dict__)
